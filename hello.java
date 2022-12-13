70d93a1c02523c7b546062c105a5da8ae54a236b
ManasaDeshpande12 <119603404+ManasaDeshpande12@users.noreply.github.com>
2022-12-14 02:15:06 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,95 @@
-classSimple{  
+pipeline{
 
-    publicstatic void main(Stringargs[]){  
 
-     System.out.println("Hello Java");  
+agent any
+
+stages('CI CD Pipeline'){
+
+
+  stage('Code Checkout'){
+
+
+    steps{
+
+
+        script{
+
+            git credentialsId: '0785a37c-780c-4b71-9322-8f503b77cc1b', url: 'https://gitlab.com/05manasa12/Firstproject1.git'
+
+
+            } 
+
+        }
+
+
+    }
+
+ 
+
+ 
+
+  stage('Build'){
+
+ 
+
+    steps{
+
+ 
+
+        script{
+
+ 
+
+            sh "mvn clean install -Dmaven.test.skip=true"
+
+ 
+
+        }
+
+ 
 
     }  
 
-} 
+ 
+
+  }
+
+ 
+
+  stage('Test'){
+
+ 
+
+    steps{
+
+ 
+
+        script{
+
+ 
+
+            sh "mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Pcoverage-per-test"
+
+ 
+
+        }
+
+ 
+
+    }  
+
+ 
+
+  }
+
+ 
+
+  
+
+ 
+
+}
+
+ 
+
+}
